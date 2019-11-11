af6f441609c8ea0a11bd72ebbff90582494fd41c
abelhasratyan <abelhasratyan@gmail.com>
2019-11-11 12:52:35 +0400

    add body parser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 const passport = require('passport')
+const bodyParser = require("body-parser");
 
 const indexRouter = require('./routes/index');
 
@@ -18,6 +19,10 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
 app.use("/", express.static(__dirname + '/view'));
